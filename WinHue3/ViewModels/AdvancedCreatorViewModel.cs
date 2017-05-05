cd0042aa304c8246a7e7b496d4e53f2531fd41e6
Hyrules <hyrules@gmail.com>
2017-05-05 13:00:47 -0400

    Continued the advance creator.

--- WinHue3/ViewModels/AdvancedCreatorViewModel.cs
+++ WinHue3/ViewModels/AdvancedCreatorViewModel.cs
@@ -43,25 +43,87 @@ namespace WinHue3.ViewModels
 
         private void CreateSensorTemplate()
         {
+            Type = "sensor";
+            Text = @"{
+    ""name"" : ""YOUR NAME"",
+    ""description"" : ""YOUR DESCRIPTION"",
+    ""modelid"": """".
+    ""swversion"": """",
+    ""type"": """",
+    ""uniqueid"": """"
+    ""manufacturername"": ""
+    ""state"":{
+        ...
+    }
+}";
         }
 
         private void CreateRuleTemplate()
         {
+            Type = "rule";
+            Text = @"{  
+    ""name"":""YOUR NAME"",
+    ""conditions"":[
+        {
+            ""address"":""/sensors/2/state/buttonevent"",
+            ""operator"":""eq"",
+            ""value"":""16""
+        },
+        {
+            ...
+        }
+    ],
+    ""actions"":[
+        {
+            ""address"":""/groups/0/action"",
+            ""method"":""PUT"", 
+            ""body"":{
+                ""scene"":""S3""
+            }
+        },
+        {
+            ...
+        }
+    ]
+}";
         }
 
-        private void CreateRessourceLinkTemplate()
+        private void CreateResourceLinkTemplate()
         {
+            Type = "resourcelink";
+            Text = @"{
+    ""name"": ""Sunrise"",
+    ""description"": ""Carla's wakeup experience"",
+    ""type"":""Link"",
+    ""class"": 1,
+    ""owner"": ""78H56B12BAABCDEF"",
+    ""links"": [""/schedules/2"", ""/schedules/3"",
+              ""/scenes/ABCD"", ""/scenes/EFGH"", ""/groups/8""]
+    }
+";
         }
 
         private void CreateGroupTemplate()
         {
+            Type = "group";
+            Text = @"{
+    ""name"": ""Living room"",
+    ""type"": ""Room"",
+    ""class"": ""Living room"",
+    ""lights"": [
+        ""3"",
+        ""4""
+    ]
+    }
+
+";
         }
 
 
         public ICommand CreateScheduleTemplateCommand => new RelayCommand(param => CreateScheduleTemplate(), (param) => Type == string.Empty);
         public ICommand CreateSensorTemplateCommand => new RelayCommand(param => CreateSensorTemplate(), (param) => Type == string.Empty);
         public ICommand CreateRuleTemplateCommand => new RelayCommand(param => CreateRuleTemplate(), (param) => Type == string.Empty);
-        public ICommand CreateRessourceLinkTemplateCommand => new RelayCommand(param => CreateRessourceLinkTemplate(), (param) => Type == string.Empty);
+        public ICommand CreateResourceLinkTemplateCommand => new RelayCommand(param => CreateResourceLinkTemplate(), (param) => Type == string.Empty);
         public ICommand CreateGroupTemplateCommand => new RelayCommand(param => CreateGroupTemplate(), (param) => Type == string.Empty);
 
         public string Text
