c2f1b709cd2aa6a42f7f3586f6941a425ee0af18
Pascal Pharand <pharandp@hotmail.com>
2017-06-09 06:31:22 -0400

    Continue work on rule creator

---
+++ WinHue3/Utils/RuleTreeViewItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using HueLib2;
+
+namespace WinHue3.Utils
+{
+    public class RuleTreeViewItem
+    {
+        private List<RuleTreeViewItem> _childrens;
+        private string _name;
+        private PropertyInfo _prop;
+        private object _hueObject;
+        private string _path;
+
+        public RuleTreeViewItem()
+        {
+            Name = string.Empty;
+            _childrens = new List<RuleTreeViewItem>();
+        }
+
+        public List<RuleTreeViewItem> Childrens
+        {
+            get { return _childrens; }
+            set { _childrens = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public PropertyInfo Property
+        {
+            get { return _prop; }
+            set { _prop = value; }
+        }
+
+        public object HObject
+        {
+            get { return _hueObject; }
+            set { _hueObject = value; }
+        }
+
+        public string Path
+        {
+            get { return _path; }
+            set { _path = value; }
+        }
+    }
+}
