7d080b3579408d7cfd60336cacaab7fc0279d650
Pascal Pharand <pharandp@hotmail.com>
2017-08-01 18:41:38 -0400

    Huelib2 merged with WinHue3. Rework most of the code. Started Implementing async fonctions. Creator wont work at this point

---
+++ WinHue3/Utils/AsyncCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WinHue3.Utils
+{
+    class AsyncRelayCommand : ICommand
+    {
+        private Func<object, Task> _action;
+        private Task _task;
+
+        public AsyncRelayCommand(Func<object, Task> action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _task == null || _task.IsCompleted;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public async void Execute(object parameter)
+        {
+            _task = _action(parameter);
+            OnCanExecuteChanged();
+            await _task;
+            OnCanExecuteChanged();
+        }
+
+        private void OnCanExecuteChanged()
+        {
+            var handler = this.CanExecuteChanged;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+    }
+}
