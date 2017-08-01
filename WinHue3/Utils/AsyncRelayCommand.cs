f51983c61e9e14a33ea046891dfc9a103bcc9485
Pascal Pharand <pharandp@hotmail.com>
2017-08-01 18:42:51 -0400

    Added missing AsyncRelayCommand

---
+++ WinHue3/Utils/AsyncRelayCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WinHue3.Utils
+{
+    public class AsyncRelayCommand : ICommand
+    {
+        private readonly Func<object, Task> execute;
+        private readonly Func<object, bool> canExecute;
+
+        private long isExecuting;
+
+        public AsyncRelayCommand(Func<object, Task> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute ?? (o => true);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (Interlocked.Read(ref isExecuting) != 0)
+                return false;
+
+            return canExecute(parameter);
+        }
+
+        public async void Execute(object parameter)
+        {
+            Interlocked.Exchange(ref isExecuting, 1);
+            RaiseCanExecuteChanged();
+
+            try
+            {
+                await execute(parameter);
+            }
+            finally
+            {
+                Interlocked.Exchange(ref isExecuting, 0);
+                RaiseCanExecuteChanged();
+            }
+        }
+    }
+}
