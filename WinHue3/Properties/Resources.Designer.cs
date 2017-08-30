c9a125ef49d3c4d8865e19dc5ae6d1e74c48767f
banksio <nathantb3@gmail.com>
2017-08-30 20:16:06 +0100

    Changed ribbon icon for sensor creation to be 32px for correct scaling size.

--- WinHue3/Properties/Resources.Designer.cs
+++ WinHue3/Properties/Resources.Designer.cs
@@ -493,6 +493,16 @@ namespace WinHue3.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap sensor2 {
+            get {
+                object obj = ResourceManager.GetObject("sensor2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap SensorSearch {
             get {
                 object obj = ResourceManager.GetObject("SensorSearch", resourceCulture);
