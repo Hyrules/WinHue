1f51edc85c65a58467ee8bdf9e5643bb0f3825e4
Hyrules <hyrules@gmail.com>
2017-08-30 18:54:51 -0400

    Started Unknown serializer.

--- WinHue3/Philips Hue/HueObjects/SensorObject/UnknownSensorObject/UnknownSensor.cs
+++ WinHue3/Philips Hue/HueObjects/SensorObject/UnknownSensorObject/UnknownSensor.cs
@@ -158,4 +158,22 @@ namespace WinHue3.Philips_Hue.HueObjects.SensorObject.UnknownSensorObject
             throw new NotImplementedException();
         }
     }
+
+    public class SensorJsonConverter:JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
