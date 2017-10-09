08ca4dd3d64e67b050ac0bca52b0f722c22cd498
Hyrules <hyrules@gmail.com>
2017-10-08 21:02:07 -0400

    Tentative RC3

---
+++ WinHue3/Utils/StringCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinHue3.Utils
+{
+    [Serializable]
+    public class StringCollection : List<string>
+    {
+        public override string ToString()
+        {
+            return string.Join(",", this);
+        }
+    }
+}
