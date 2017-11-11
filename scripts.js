7fbafdaa5c0f2e272e7037cd89644aba3b8e06d7
banksio <nathantb3@gmail.com>
2017-11-11 00:11:29 +0000

    Experimenting with layout, sliders etc.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+
+$(document).ready(function(){
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true
+      });
+});
