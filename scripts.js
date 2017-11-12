991c0a317a5a9b085788933462f950d9bc97739f
banksio <nathantb3@gmail.com>
2017-11-12 18:39:45 +0000

    Added magnific popups

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+  $('[data-toggle="tooltip"]').tooltip()
+})
 
-
-$(document).ready(function(){
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true
-      });
+$(document).ready(function() {
+  $('.image-link').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+      beforeOpen: function() {
+        // just a hack that adds mfp-anim class to markup 
+         this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+         this.st.mainClass = this.st.el.attr('data-effect');
+      }
+    },
+  });
 });
+
