647c08512af6fd84f6c926d7509d3a2491aac918
Hyrules <hyrules@gmail.com>
2017-11-15 13:25:41 -0500

    started behavior for treeview

---
+++ WinHue3/Functions/Schedules/NewCreator/TreeViewBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace WinHue3.Functions.Schedules.NewCreator
+{
+    public class TreeViewBehavior : Behavior<UIElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+        }
+    }
+}
