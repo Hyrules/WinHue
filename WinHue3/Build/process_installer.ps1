7885cf4c37534ff706d2d091a39b0028151b70d6
Hyrules <hyrules@gmail.com>
2018-01-02 16:50:43 -0500

    Added processing files to generate installer file and readme

---
+++ WinHue3/Build/process_installer.ps1
@@ -0,0 +1,9 @@
+$installer = $args[0];
+$project = $args[1];
+$path = Split-Path -Path $installer
+
+$content = Get-Content -Path $installer -Encoding UTF8
+$content = $content.Replace("[[[WINHUE3]]]",$project)
+$utf8 = New-Object System.Text.UTF8Encoding $false
+[System.IO.File]::WriteAllLines("$path\\installer.iss",$content,$utf8);
+#$content | Out-File -FilePath "$path\\installer.iss" -Force
