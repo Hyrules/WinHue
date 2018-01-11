23985d3830b2cf7bc6d5f87d3f525b517cdd7a00
Hyrules <hyrules@gmail.com>
2018-01-11 06:30:35 -0500

    Started LIFX bulb finder

---
+++ WinHue3/LIFX/Finder/LifxFinderViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WinHue3.LIFX.Responses;
+using WinHue3.LIFX.Responses.States.Device;
+using WinHue3.Utils;
+
+namespace WinHue3.LIFX.Finder
+{
+    public class LifxFinderViewModel : ValidatableBindableBase
+    {
+
+
+        public LifxFinderViewModel()
+        {
+            
+        }
+
+        public ICommand FindLifxDeviceCommand => new AsyncRelayCommand(param => FindLifxDevice(), (param) => CanFindLifxDevice());
+
+        private bool CanFindLifxDevice()
+        {
+            
+        }
+
+        private async Task FindLifxDevice()
+        {
+            LifxCommMessage<Dictionary<IPAddress,StateService>> response = await Lifx.GetDevicesAsync();
+
+            if (response.Error == false)
+            {
+                foreach (KeyValuePair<IPAddress, StateService> kvp in response.Data)
+                {
+
+                }
+            }
+
+        }
+    }
+}
