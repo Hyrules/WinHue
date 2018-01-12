170ecd72e2d9e31a25a1ad9bb9708749bb15ecce
banksio <nathantb3@gmail.com>
2018-01-12 23:39:07 +0000

    Add old assets

---
+++ _layouts/scripts.js
@@ -0,0 +1,39 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+$(document).ready(function() {
+  $('.image-link').magnificPopup({
+    type: 'image',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    image: {
+      cursor: 'null',
+    },
+    callbacks: {
+      beforeOpen: function() {
+        // just a hack that adds mfp-anim class to markup 
+         this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+         this.st.mainClass = this.st.el.attr('data-effect');
+      }
+    },
+    closeOnContentClick: true,
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+});
+
+function datalol(reponse)
+{
+  alert(response)
+}
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
