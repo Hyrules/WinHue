20d8701dbbd0e164dfd374ed15151468f466edcb
Hyrules <hyrules@gmail.com>
2018-11-29 13:39:57 -0500

    working on the parser

--- WinHue3/Functions/Animations2/WinHueAnimationParser.cs
+++ WinHue3/Functions/Animations2/WinHueAnimationParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Sprache;
@@ -14,7 +15,6 @@ namespace WinHue3.Functions.Animations2
     {
         private static readonly Parser<char> SpaceDelimiter = Parse.WhiteSpace;
         private static readonly Parser<char> LineDelimiter = Parse.Char(';');
-        private static readonly Parser<string> Light = Parse.String("LIGHT");
 
         private static readonly Parser<int> WaitCommand =
             from wait in Parse.String("WAIT")
@@ -23,13 +23,18 @@ namespace WinHue3.Functions.Animations2
             select Convert.ToInt32(value);
 
         private static readonly Parser<Tuple<string, string>> setter =
-            from typeword in Parse.Letter.Many().Text()
+            from typeword in (Parse.String("LIGHT").Or(Parse.String("GROUP"))).Text()
             from space in SpaceDelimiter
             from id in Parse.Digit.DelimitedBy(SpaceDelimiter).Text()
             select new Tuple<string, string>(typeword, id);
 
         private static readonly Parser<KeyValuePair<string, string>> property =
-            from prop in Parse.Letter.Many().Text()
+            from prop in (
+                Parse.String("BRI")
+                .Or(Parse.String("SAT")               
+                .Or(Parse.String("CT")
+                .Or(Parse.String("HUE")
+                .Or(Parse.String("ON")))))).Text()
             from sep in Parse.Char(':')
             from val in Parse.Digit.Many().Text()
             select new KeyValuePair<string, string>(prop, val);
@@ -39,12 +44,33 @@ namespace WinHue3.Functions.Animations2
             from lineend in LineDelimiter
             select new Dictionary<string, string>(prop.ToDictionary(x => x.Key,x => x.Value));
 
-        private static readonly Parser<string> 
+        private static readonly Parser<IHueObject> hueobject =
+            from set in Parse.String("SET")
+            from space1 in SpaceDelimiter
+            from type in setter
+            from space2 in SpaceDelimiter
+            from to in Parse.String("TO")
+            from space3 in SpaceDelimiter
+            from props in properties
+            select CreateHueObject(type,props);
+
+        private static IHueObject CreateHueObject(Tuple<string,string> type ,Dictionary<string,string> properties)
+        {
+            IHueObject ho = HueObjectCreator.CreateHueObject(type.Item1);
+            ho.Id = type.Item2;
+            PropertyInfo[] pi = typeof(State).GetProperties();
+
+            foreach(KeyValuePair<string,string> kvp in properties)
+            {
+
+            }
+            return ho;
+        }
 
         public static object ParseAnimation(string text)
         {
         
-            return properties.Parse(text);
+            return hueobject.Parse(text);
         }
     }
 }
