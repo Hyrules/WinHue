8b315155c1d70ae51f8670fe90b1e2a59e5a4da8
Nathan Banks <nathantb3@gmail.com>
2019-01-18 23:35:09 +0000

    Small work on theming

--- WinHue3/Resources/GUI.Designer.cs
+++ WinHue3/Resources/GUI.Designer.cs
@@ -19,7 +19,7 @@ namespace WinHue3.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class GUI {
@@ -2986,7 +2986,7 @@ namespace WinHue3.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Add to rule actions.
+        ///   Looks up a localized string similar to Add.
         /// </summary>
         public static string RuleCreatorForm_AddToRuleActions {
             get {
