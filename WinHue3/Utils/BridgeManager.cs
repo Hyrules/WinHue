1cd5c3e0e8984a481775169e2b2eda0e35359469
Hyrules <hyrules@gmail.com>
2019-02-04 15:23:00 -0500

    Started creating the bridge manager.

---
+++ WinHue3/Utils/BridgeManager.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WinHue3.Functions.Application_Settings.Settings;
+using WinHue3.Functions.BridgeFinder;
+using WinHue3.Functions.BridgePairing;
+using WinHue3.Philips_Hue.BridgeObject;
+using WinHue3.Philips_Hue.BridgeObject.BridgeMessages;
+using WinHue3.Philips_Hue.BridgeObject.BridgeObjects;
+
+namespace WinHue3.Utils
+{
+    public static class BridgeManager
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static List<Bridge> _listBridges;
+        private static Bridge _selectedBridge;
+
+        public static event EventHandler OnBridgeRemoved;
+        public static event EventHandler OnBridgeAdded;
+        public static event EventHandler OnSelectedBridgeChanged;
+
+        public static event BridgeNotResponding OnBridgeNotResponding;
+        public delegate void BridgeNotResponding(object sender, BridgeNotRespondingEventArgs e);
+
+        public static event BridgeAdded OnBridgeMessageAdded;
+        public delegate void BridgeAdded(object sender, MessageAddedEventArgs e);
+
+        static BridgeManager()
+        {
+            _listBridges = new List<Bridge>(); 
+        }
+
+        public static Bridge SelectedBridge => _selectedBridge;
+
+        public static bool DoBridgePairing()
+        {
+            Form_BridgeDetectionPairing dp = new Form_BridgeDetectionPairing(_listBridges) { Owner = Application.Current.MainWindow };
+            bool result = (bool)dp.ShowDialog();
+            if (!result) return result;
+            _listBridges = dp.ViewModel.ListBridges.ToList();
+            SaveSettings();
+            return result;
+        }
+
+        private static bool CheckBridge(Bridge bridge)
+        {
+            log.Info("Checking if ip is bridge...");
+            BasicConfig bc = bridge.GetBridgeBasicConfig();
+            if (bc != null)
+            {
+                bridge.ApiVersion = bc.apiversion;
+                bridge.Name = bc.name;
+                bridge.SwVersion = bc.swversion;
+                WinHueSettings.bridges.BridgeInfo[bridge.Mac].name = bridge.Name;
+                WinHueSettings.SaveBridges();
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool SaveSettings()
+        {
+            foreach (Bridge br in _listBridges)
+            {
+                if (br.Mac == string.Empty) continue;
+                if (WinHueSettings.bridges.BridgeInfo.ContainsKey(br.Mac))
+                    WinHueSettings.bridges.BridgeInfo[br.Mac] = new BridgeSaveSettings
+                    {
+                        ip = br.IpAddress.ToString(),
+                        apikey = br.ApiKey,
+                        name = br.Name,
+                    };
+                else
+                    WinHueSettings.bridges.BridgeInfo.Add(br.Mac,
+                        new BridgeSaveSettings { ip = br.IpAddress.ToString(), apikey = br.ApiKey, name = br.Name });
+
+                if (br.IsDefault) WinHueSettings.bridges.DefaultBridge = br.Mac;
+            }
+
+            return WinHueSettings.SaveBridges();
+        }
+
+        public static void LoadBridges()
+        {
+            while (true)
+            {
+                log.Info("Loading bridge(s)...");
+
+                log.Info($"Checking if any bridge already present in settings... found {WinHueSettings.bridges.BridgeInfo.Count}");
+
+                if (WinHueSettings.bridges.BridgeInfo.Count == 0 || WinHueSettings.bridges.DefaultBridge == string.Empty || !WinHueSettings.bridges.BridgeInfo.ContainsKey(WinHueSettings.bridges.DefaultBridge))
+                {   // No bridge found in the list of bridge.
+                    log.Info("Either no bridge found in settings or no default bridge. Pairing needed.");
+                    if (DoBridgePairing())
+                        continue;
+                    else
+                        break;
+                }
+
+                foreach (KeyValuePair<string, BridgeSaveSettings> b in WinHueSettings.bridges.BridgeInfo)
+                {
+                    log.Info($"Bridge OK. Checking if bridge already in the bridge list...");
+                    if (_listBridges.All(x => x.Mac != b.Key))
+                    {
+                        Bridge bridge = new Bridge()
+                        {
+                            ApiKey = b.Value.apikey,
+                            IpAddress = IPAddress.Parse(b.Value.ip),
+                            Name = b.Value.name,
+                            IsDefault = b.Key == WinHueSettings.bridges.DefaultBridge,
+                            Mac = b.Key
+                        };
+                        if (b.Value.apikey == string.Empty) continue;
+
+                        bridge.LastCommandMessages.OnMessageAdded += LastCommandMessages_OnMessageAdded;
+                        bridge.RequiredUpdate = WinHueSettings.settings.CheckForBridgeUpdate && UpdateManager.CheckBridgeNeedUpdate(bridge.ApiVersion);
+
+                        log.Info("Bridge not in the list adding it...");
+                        _listBridges.Add(bridge);
+
+                    }
+                    else
+                    {
+                        log.Info("Bridge already in the list skipping...");
+                    }
+                }
+
+                foreach (Bridge br in _listBridges)
+                {
+                    log.Info($"Checking bridge {br}");
+                    if (!CheckBridge(br))
+                    {
+                        log.Info("Bridge IP has changed... Pairing needed.");
+                        Form_BridgeFinder fbf = new Form_BridgeFinder(br) { Owner = Application.Current.MainWindow };
+                        fbf.ShowDialog();
+
+                        if (fbf.IpFound())
+                        {
+                            br.BridgeNotResponding += Br_BridgeNotResponding;
+                            br.IpAddress = fbf.newip;
+                            if (!br.IsDefault) continue;
+                            _selectedBridge = br;
+                        }
+                        else
+                        {
+                            DoBridgePairing();
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (!br.IsDefault) continue;
+                        _selectedBridge = br;
+                    }
+                }
+
+                // TODO : PLACE IN THE MAINFORM VIEW MODEL
+                /*
+                 
+                if (SelectedBridge != null)
+                {
+                    _ctm = new CpuTempMonitor(SelectedBridge);
+
+                }
+                */
+
+                break;
+            }
+        }
+
+        private static void Br_BridgeNotResponding(object sender, BridgeNotRespondingEventArgs e)
+        {
+            OnBridgeNotResponding?.Invoke(sender, e);
+        }
+
+        private static void LastCommandMessages_OnMessageAdded(object sender, MessageAddedEventArgs e)
+        {
+            OnBridgeMessageAdded?.Invoke(sender, e);
+        }
+    }
+}
