c930e4f66de2b48175653ae6c4eec11b17722d54
Hyrules <hyrules@gmail.com>
2020-01-02 09:54:28 -0500

    added missing file

---
+++ WinHue3/Utils/UnreachableAdorner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Drawing;
+using System.Windows.Media;
+
+namespace WinHue3.Utils
+{
+    public class UnreachableAdorner : Adorner
+    {
+        public UnreachableAdorner(UIElement adornedElement) : base(adornedElement)
+        {
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            Rect adornedElementRect = new Rect(this.AdornedElement.DesiredSize);
+
+            SolidColorBrush renderBrush = new SolidColorBrush(System.Windows.Media.Colors.Black) { Opacity = 0.1 };
+        }
+    }
+}
